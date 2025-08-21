317e7f29bd7569df690d0f7b2ea33e4bc7c90683
Daniel Ibarrola <daniel.ibarrola.sanchez@gmail.com>
2025-08-21 11:02:20 -0600

    github oidc provider

--- infrastructure/global/iam/main.tf
+++ infrastructure/global/iam/main.tf
@@ -30,6 +30,13 @@ resource "aws_iam_user" "s3_upload_user" {
     Description = "User for uploading objects to CIRES report buckets"
     Environment = "Stage and Production"
   }
+
+  lifecycle {
+    ignore_changes = [
+      tags,
+      tags_all,
+    ]
+  }
 }
 
 data "aws_iam_policy_document" "s3_upload_policy_doc" {
@@ -71,4 +78,176 @@ resource "aws_iam_policy" "s3_upload_policy" {
 resource "aws_iam_user_policy_attachment" "s3_upload_policy_attachment" {
   user       = aws_iam_user.s3_upload_user.name
   policy_arn = aws_iam_policy.s3_upload_policy.arn
+}
+
+# Create an IAM OIDC identity provider that trusts GitHub
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    data.tls_certificate.github.certificates[0].sha1_fingerprint
+  ]
+}
+
+# Fetch GitHub's OIDC thumbprint
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+
+locals {
+  github_owner = "Daniel-Ibarrola"
+  allowed_repos_branches = [
+    {
+      repo = "servicios-cires"
+      branch = "main"
+    },
+    {
+      repo = "servicios-cires"
+      branch = "develop"
+    }
+  ]
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+      type        = "Federated"
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      # The repos and branches defined in allowed_repos_branches
+      # will be able to assume this IAM role
+      values = [
+        for a in local.allowed_repos_branches :
+        "repo:${local.github_owner}/${a["repo"]}:ref:refs/heads/${a["branch"]}"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_terraform_role" {
+  name = "github-actions-terraform-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+
+data "aws_iam_policy_document" "terraform_permissions" {
+  # Permissions for the Terraform State Backend
+  statement {
+    sid    = "TerraformState"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::infrastructure-state-servicios-cires",
+      "arn:aws:s3:::infrastructure-state-servicios-cires/*"
+    ]
+  }
+
+  statement {
+    sid    = "TerraformStateLock"
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/infrastructure-servicios-cires-lock"]
+  }
+
+  statement {
+    sid    = "IAM"
+    effect = "Allow"
+    actions = [
+      "iam:CreateRole",
+      "iam:GetRole",
+      "iam:DeleteRole",
+      "iam:TagRole",
+      "iam:CreateUser",
+      "iam:GetUser",
+      "iam:DeleteUser",
+      "iam:TagUser",
+      "iam:CreatePolicy",
+      "iam:GetPolicy",
+      "iam:DeletePolicy",
+      "iam:AttachRolePolicy",
+      "iam:DetachRolePolicy",
+      "iam:AttachUserPolicy",
+      "iam:DetachUserPolicy",
+      "iam:PassRole"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "S3"
+    effect = "Allow"
+    actions = [
+      "s3:CreateBucket",
+      "s3:DeleteBucket",
+      "s3:GetBucketLocation",
+      "s3:PutLifecycleConfiguration",
+      "s3:GetLifecycleConfiguration",
+      "s3:PutBucketPublicAccessBlock",
+      "s3:GetBucketPublicAccessBlock",
+      "s3:PutBucketNotification",
+      "s3:GetBucketNotificationConfiguration",
+      "s3:PutBucketPolicy",
+      "s3:GetBucketPolicy",
+      "s3:DeleteBucketPolicy"
+    ]
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+  }
+
+  statement {
+    sid    = "Lambda"
+    effect = "Allow"
+    actions = [
+      "lambda:CreateFunction",
+      "lambda:GetFunction",
+      "lambda:DeleteFunction",
+      "lambda:UpdateFunctionConfiguration",
+      "lambda:UpdateFunctionCode",
+      "lambda:AddPermission",
+      "lambda:RemovePermission"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "CloudTrail"
+    effect = "Allow"
+    actions = [
+      "cloudtrail:CreateTrail",
+      "cloudtrail:DeleteTrail",
+      "cloudtrail:GetTrail",
+      "cloudtrail:UpdateTrail",
+      "cloudtrail:PutEventSelectors"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "terraform_permissions_policy" {
+  name        = "terraform-permissions-policy"
+  description = "Permissions for the GitHub Actions Terraform role."
+  policy      = data.aws_iam_policy_document.terraform_permissions.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "terraform_role_attachment" {
+  role       = aws_iam_role.github_actions_terraform_role.name
+  policy_arn = aws_iam_policy.terraform_permissions_policy.arn
 }
